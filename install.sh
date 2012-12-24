fcdec3eb9abbac16410bc150f9590772af924ab5
yinsigan <ganweiliang886@gmail.com>
2012-12-24 18:09:57 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get install ctags grep-ack
 cd ~
-git clone https://github.com/yinsigan/my_vim_config.git
-ln -s ~/my_vim_config/.vimrc ~/.vimrc
-ln -s ~/my_vim_config/.vimrc.map ~/.vimrc.map
-ln -s ~/my_vim_config/.vimrc.plugins ~/.vimrc.plugins
-ln -s ~/my_vim_config/.vimrc.bundle ~/.vimrc.bundle
+git clone git://github.com/yinsigan/vimconf.git
+ln -s ~/vimconf/.vimrc ~/.vimrc
+ln -s ~/vimconf/.vimrc.map ~/.vimrc.map
+ln -s ~/vimconf/.vimrc.plugins ~/.vimrc.plugins
+ln -s ~/vimconf/.vimrc.bundle ~/.vimrc.bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall! +BundleClean +q
